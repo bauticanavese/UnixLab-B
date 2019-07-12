8d0a355a312d80d3407441d2b8592899eb482f74
bauticanavese <bauti-canavese@hotmail.com>
2019-07-12 16:44:36 -0300

    Algunas correcciones

--- code/cat0.c
+++ code/cat0.c
@@ -10,18 +10,15 @@
 // Pre: solo se pasa un archivo, este archivo existe y se tienen 
 // permisos de lectura.
 void cat0(const char *file) {
-	int fd, fd_out;
 	ssize_t read = -1, written = -1;
 	char buffer[BUFF_SIZE];
-
-	fd = open(file, O_RDONLY);
-	fd_out = fileno(stdout);
+	int fd = open(file, O_RDONLY);
 
 	while (read != 0 && written != 0) {
 		if ((read = _read(fd, buffer, BUFF_SIZE)) == -1) {
 			err(EXIT_FAILURE, NULL);
 		}
-		if ((written = _write(fd_out, buffer, (size_t)read)) == -1) {
+		if ((written = _write(STDOUT_FILENO, buffer, (size_t)read)) == -1) {
 			err(EXIT_FAILURE, NULL);
 		}
 	}
